1a79fd029756327e8abf395f198ddd5d922cd6a0
LAP13694-local <duoc1706@gmail.com>
2021-01-14 19:39:54 +0700

    Baitap1

---
+++ src/com/company/Webbaitap16.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Webbaitap16 {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        System.out.print("Nhập số tự nhiên n: "); int n = nhap.nextInt();
+        System.out.print("Nhập số tự nhiên x: "); int x = nhap.nextInt();
+        double tong = 0; double mauso = 0; int mu = 0;
+        for (int i=1; i<=n; i++){
+            mu += i;
+            tong += Math.pow(x,mu)/mu;
+        }
+        System.out.printf("Tổng S là %.3f",tong);
+}}
