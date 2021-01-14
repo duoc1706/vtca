8884e66e0136a549d5265b630b30a9667ed8108d
LAP13694-local <duoc1706@gmail.com>
2021-01-14 20:23:42 +0700

    Baitap1

---
+++ src/com/company/Timvitrisolonnhat.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Timvitrisolonnhat {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        int a[] = new int[]{11, 12, 15, 14, 13, 16, 18, 17};
+        int max = a[0];
+        int vitri = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (max < a[i]){
+                max=a[i];
+                vitri = i;}
+        }
+        System.out.println("Vị trí max là " + vitri);
+    }
+}
