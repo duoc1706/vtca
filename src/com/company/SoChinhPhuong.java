0ff6d8d9099e36aa29b7a483639a4a9af996fd2e
LAP13694-local <duoc1706@gmail.com>
2021-01-16 21:00:17 +0700

    1

---
+++ src/com/company/SoChinhPhuong.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class SoChinhPhuong {
+    public static void main(String[] args) {
+        int n = 144;
+        if (check(n)) {
+            System.out.println(n + " là số chính phương"); }
+        else System.out.println(n + " không phải là số chính phương");
+    }
+    static boolean check(int n){
+        boolean ketqua = false;
+        if (Math.sqrt(n) - (int) Math.sqrt(n)==0) {ketqua = true;}
+        return ketqua;
+    }
+}
