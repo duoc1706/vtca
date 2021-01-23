6d916239ba163146447b2fb19a69dc6774fc8e74
LAP13694-local <duoc1706@gmail.com>
2021-01-23 20:08:31 +0700

    2

--- src/com/company/MainProduct.java
+++ src/com/company/MainProduct.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MainProduct {
     public static void main(String[] args) {
         Product product[] = new Product[5];
-        String sanpham; int soluong; int giaban;
+        String sanpham; int soluong; int giaban; int sumtong =0;
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < product.length; i++) {
             sc.nextLine();
@@ -22,10 +22,12 @@ public class MainProduct {
         System.out.println("DANH MỤC SẢN PHẨM");
         System.out.println("|STT|  Tên sản phẩm  | Số lượng  |  Đơn giá  |  Tổng tiền  |");
         for (int i = 0; i < product.length; i++) {
+            sumtong += product[i].getTongtien()
             System.out.printf("|%3d|%16s|%11d|%11d|%13d|",(i+1),product[i].getTensanpham(),
             product[i].getSoluong(),product[i].getGiaban(),
                     product[i].getTongtien());
-            System.out.println();
+            System.out.println(); \
+            System.out.printf("|%54d|",sumtong);
         }
     }
 }
